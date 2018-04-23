36e412601a9179c03935ddfe959cab37cc00b2d1
brashikje <brashikje@mail.ru>
2018-04-23 12:02:02 +0400

    Создание класса

---
+++ laba1/laba1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Student
+    {
+        private string name;
+        private int kurs;
+        private bool pol;
+        /// <summary>
+        /// Инициализирует новый экземпляр класса Student значением по умолчанию
+        /// </summary>
+    }
+}
